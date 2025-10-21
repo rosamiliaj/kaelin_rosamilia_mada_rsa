b5fcd69d2c399599db6c04db12aabedd78f2f4b9
janro <jan.rosamilia@students.fhnw.ch>
2025-10-21 14:27:18 +0200

    Main Klasse

--- Projekt/src/RSAApplication.java
+++ Projekt/src/RSAApplication.java
@@ -1,9 +1,33 @@
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class RSAApplication {
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("RSA-Programm!");
+        System.out.println("*******************************************************");
+        System.out.println("Wähle aus:");
+        System.out.println("1 = generate (Schlüssel erzeugen)");
+        System.out.println("2 = encrypt (Text verschlüsseln)");
+        System.out.println("3 = decrypt (Text entschlüsseln)");
+        System.out.print("Deine Auswahl: ");
+        int auswahl = sc.nextInt();
+
+    }
+
+
+    public void generate(){
+
+    }
+
+    public void encrypt(){
+
+    }
+    public void decrypt(){
+
     }
 
 }
